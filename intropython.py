51262f5c3b04238fbfb958ee34176cc38aa761a0
Kevin <santiago2gomez1@gmail.com>
2024-03-20 15:23:27 -0500

    operadores

--- intropython.py
+++ intropython.py
@@ -1,1 +1,26 @@
 #comentario de una linea todo lo que va despues de la linea es ignorado por el interprte de python
+#variables: espacio de memoria, con nombre , donde guardo vvalores 
+#los nombres de variables deben ser cortos, descriptivos, no tener espacios en blanco ni caracteres especiales, no deben empezar por un numero.
+
+#descriptivo significa que representa la catgoria del dato que quiero guardar
+#las variables pueden tener cualquier dato de tipo primitivo 
+#numeros (entero, real), cadenas de caracteres (string), boleanos (true, false)
+#caracteres (letras).
+
+variable=1
+variable = 'juventud divino tesoro, te vas para no volver'
+variable= True
+variable='a'
+variable =3.1415926535
+#para asignar valor a la variable se usa el operador =
+
+#operadores:mecanismo para obtener un dato apartir de otros datos.
+#los datos que intervienen se llaman operandos.
+#aritmeticos: +,-,/,*,%
+#de comparacion: retornan true o false.<> >= <= == !=
+#de logica booleana :OR AND. Retornan True o False de acuerdo a una tabla de verdad, los operadores siempre son booleanos (true o false)
+
+a=True
+b=False
+print(a and b)
+#los operadores booleanos y de comparacion son muy utilizados al definir condiciones 
